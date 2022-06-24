deacfbd00b3c7509380773bd678c08ee2ebbc598
Geovannarc <geovannarcf@gmail.com>
2022-06-24 05:42:02 -0300

    update form

--- js.js
+++ js.js
@@ -50,3 +50,10 @@ function changeContent(id){
         window.location.href = "#icone"
     }
 }
+
+document.getElementById('contatoForm').addEventListener('submit', function () {
+    var nome = this.querySelector('input[name=nome]'), nome = nome.value;
+    var email = this.querySelector('input[name=email]'), email = email.value;
+    var texto = 'Olá destinatário, \nMeu nome é '+ nome +' e meu email é '+ email;
+    this.querySelector('input[name=Body]').setAttribute('value', texto);
+});
